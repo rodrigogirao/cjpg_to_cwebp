27dc8db4c91b802c1096451a4ea187b711e0ae0f
rodrigogirao <rodrigogirao7@gmail.com>
2015-04-17 21:44:08 -0300

    Converting png files

--- cjpg_to_cwebp_converter.sh
+++ cjpg_to_cwebp_converter.sh
@@ -5,13 +5,32 @@ IFS='';
 while read line; do
 	cd $line;
 	echo "========== $line ==========";
+	for f in *.cbz; do	
+		unzip ./"$f";
+		for jf in *.jpg; do
+			cwebp ./"$jf" -o ./"${jf%.jpg}.webp";
+			rm ./"$jf";
+		done
+		for pf in *.png; do
+			cwebp ./"$pf" -o ./"${pf%.png}.webp";
+			rm ./"$pf";
+		done
+		zip -r ./"${f%.cbz}_webp.cbz" *.webp;
+		rm -r *.webp;
+		#rm ./"$f";
+	done
+	
 	for f in *.cbr; do	
 		unzip ./"$f";
 		for jf in *.jpg; do
 			cwebp ./"$jf" -o ./"${jf%.jpg}.webp";
 			rm ./"$jf";
 		done
-		zip -r ./"${f%.cbr}.cbz" *.webp;
+		for pf in *.png; do
+			cwebp ./"$pf" -o ./"${pf%.png}.webp";
+			rm ./"$pf";
+		done
+		zip -r ./"${f%.cbr}_webp.cbz" *.webp;
 		rm -r *.webp;
 		#rm ./"$f";
 	done
