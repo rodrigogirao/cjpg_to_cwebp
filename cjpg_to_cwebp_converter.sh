b8885a877288e0b6eabe5ee792f5ff9ce62c9008
rodrigogirao <rodrigogirao7@gmail.com>
2015-04-17 21:30:13 -0300

    Converting files in subdirectories

--- cjpg_to_cwebp_converter.sh
+++ cjpg_to_cwebp_converter.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
-
-for f in *.cbr; do
-	unrar x ./"$f";
-	for jf in *.jpg; do
-		cwebp ./"$jf" -o ./"${jf%.jpg}.webp";
-		rm ./"$jf";
+cd "$@";
+find $(pwd) -type d -links 2 > outtemp.txt
+IFS='';
+while read line; do
+	cd $line;
+	echo "========== $line ==========";
+	for f in *.cbr; do	
+		unzip ./"$f";
+		for jf in *.jpg; do
+			cwebp ./"$jf" -o ./"${jf%.jpg}.webp";
+			rm ./"$jf";
+		done
+		zip -r ./"${f%.cbr}.cbz" *.webp;
+		rm -r *.webp;
+		#rm ./"$f";
 	done
-	zip -r ./"${f%.cbr}.cbz" *.webp;
-	rm -r *.webp
-	#rm ./"$f";
-done
-
+done < outtemp.txt
